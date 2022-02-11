71c37fda5074defb8b5bd08c3c50e266d60a1799
安藤和樹 <seal1226.jp@gmail.com>
2022-02-11 13:03:34 +0900

    add : 領収書RepositoryのIF定義。多分この三つのメソッドでええと思うんやが...

---
+++ src/Eirin.Domain/Receipts/IReceiptRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Eirin.Domain.Receipts
+{
+    public interface IReceiptRepository
+    {
+        /// <summary>
+        /// Id検索
+        /// </summary>
+        public Task<Receipt?> FindByIdAsync(ReceiptId id);
+
+        /// <summary>
+        /// 保存
+        /// </summary>
+        public Task SaveAsync(Receipt receipt);
+
+        /// <summary>
+        /// 削除
+        /// </summary>
+        public Task DeleateAsync(ReceiptId id);
+    }
+}

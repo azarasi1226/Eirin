0bb676f58a20a1414fb5f60f28e2fa06f5eb6afd
安藤和樹 <seal1226.jp@gmail.com>
2022-02-19 21:27:51 +0900

    add :GRPCサービスのDI登録

--- src/Eirin.Client/Program.cs
+++ src/Eirin.Client/Program.cs
@@ -1,4 +1,6 @@
 using Eirin.Client;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// gRPCƒ`ƒƒƒ“ƒlƒ‹‚Ì“o˜^
+builder.Services.AddScoped(services =>
+{
+    var config = services.GetRequiredService<IConfiguration>();
+    var gRPCUri = config["gRPC_URI"];
+
+    return GrpcChannel.ForAddress(gRPCUri, new GrpcChannelOptions
+    {
+        HttpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()),
+    });
+});
 
 await builder.Build().RunAsync();
--- src/Eirin.gRPCServer/Program.cs
+++ src/Eirin.gRPCServer/Program.cs
@@ -1,17 +1,26 @@
 using Eirin.gRPCServer.Services;
 
+// CORSポリシー名
+const string s_AllowAll = "AllowAll";
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddCors(o => o.AddPolicy(s_AllowAll, configure =>
+{
+    configure.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
 
 var app = builder.Build();
+// gRPC-Webを使用
+app.UseGrpcWeb();
+// CORSを使用
+app.UseCors();
+// gRPCサービスパイプライン構築
+app.MapGrpcService<GreeterService>().EnableGrpcWeb().RequireCors(s_AllowAll);
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
+app.MapGet("/", () => "gRPC専用のエンドポイントです");
 app.Run();
+
